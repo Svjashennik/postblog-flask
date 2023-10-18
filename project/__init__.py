efa5a2955f6b4d1323c1957472c947366da7e215
nikita.astashkin <nek1212121@gmail.com>
2023-10-19 01:29:01 +0300

    authorization black

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,15 @@
-from flask import Flask, send_from_directory, render_template, request, url_for, session, logging, flash, redirect, \
-    Blueprint
+from flask import (
+    Flask,
+    send_from_directory,
+    render_template,
+    request,
+    url_for,
+    session,
+    logging,
+    flash,
+    redirect,
+    Blueprint,
+)
 from flask_login import login_user, LoginManager, UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -9,17 +19,21 @@ app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
 
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
 
 @app.route("/static/<path:filename>")
 def staticfiles(filename):
     return send_from_directory(app.config["STATIC_FOLDER"], filename)
 
+
 main = Blueprint('main', __name__)
+
 
 @main.route('/about')
 def about():
@@ -30,9 +44,11 @@ def about():
 def articles():
     return render_template('main.html')
 
+
 @main.route('/pagination')
 def pagination(page):
     return render_template('main.html')
+
 
 @main.route('/blog/<blog>')
 def article():
@@ -43,11 +59,14 @@ def article():
 def display_article(id):
     return render_template('post_detail.html', id=id)
 
+
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
 
 @auth.route('/login', methods=['POST'])
 def login_post():
@@ -61,15 +80,17 @@ def login_post():
     # take the user supplied password, hash it, and compare it to the hashed password in database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login'))  # if user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
     return redirect(url_for('main.articles'))
 
+
 @auth.route('/registration')
 def signup():
     return render_template('registration.html')
+
 
 @auth.route('/registration', methods=['POST'])
 def signup_post():
@@ -82,9 +103,11 @@ def signup_post():
         flash('Пароли не совпадают')
         return redirect(url_for('auth.registration'))
 
-    user = User.query.filter_by(name=name).first() # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(
+        name=name
+    ).first()  # if this returns a user, then the email already exists in database
 
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
+    if user:  # if a user is found, we want to redirect back to signup page so user can try again
         flash('Логин уже зарегистрирован')
         return redirect(url_for('auth.registration'))
 
@@ -98,7 +121,6 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 
-
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
@@ -106,9 +128,11 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-# since the user_id is just the primary key of our user table, use it in the query for the user
+    # since the user_id is just the primary key of our user table, use it in the query for the user
     return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
+
+
 app.register_blueprint(auth)
 app.register_blueprint(main)
--- project/fixtures.py
+++ project/fixtures.py
@@ -5,21 +5,21 @@ def fixtures():
             'title': 'First article',
             'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu',
             'author': 'Sophia Iroegbu',
-            'date_created': '26-01-2022'
+            'date_created': '26-01-2022',
         },
         {
             'id': 2,
             'title': 'Second article',
             'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu',
             'author': 'John Doe',
-            'date_created': '25-01-2022'
+            'date_created': '25-01-2022',
         },
         {
             'id': 3,
             'title': 'Third article',
             'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu',
             'author': 'Mary James',
-            'date_created': '26-01-2022'
+            'date_created': '26-01-2022',
         },
     ]
 
